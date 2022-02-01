620e57a32d2133233027cebe7b13a9629b69022f
Chris Allum <ctallum@gmail.com>
2022-01-31 23:44:02 -0500

    Add Day 1

--- Day1/main.py
+++ Day1/main.py
@@ -5,7 +5,7 @@ with open('input.txt') as f:
 
 pairs = 0
 for i in range(len(data)-1):
-    if int(data[i]) == int(data[i + 1]):
+    if data[i] == data[i + 1]:
         pairs += int(data[i])
 if data[-1] == data[0]:
     pairs += int(data[0])
@@ -16,7 +16,7 @@ pairs = 0
 half = int(len(data)/2)
 for i in range(len(data)-1):
     new_index = (i + half)%len(data)
-    if int(data[i]) == int(data[new_index]):
+    if data[i] == data[new_index]:
         pairs += int(data[i])
 
 print(f'Solution to part 2: {pairs}')
