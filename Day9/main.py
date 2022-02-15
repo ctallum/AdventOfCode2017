49310dbabb9f22360bc9355dce8de93ad3d92d52
Chris Allum <ctallum@gmail.com>
2022-02-15 00:17:22 -0500

    Add Day 9

--- Day9/main.py
+++ Day9/main.py
@@ -1,44 +1,32 @@
-import sys
-from collections import defaultdict
-sys.setrecursionlimit(100000)
-
 with open('input.txt') as f:
     data = [a.strip() for a in f.readlines()]
     data = list(data[0])
 
-clean_string = []
 non_junk = 0
-
-in_garbage = False
-for idx,val in enumerate(data):
-    if val == '!':
-        data[idx+1] = '*'
-        continue
-
-    if val == '<' and not in_garbage:
-        in_garbage = True
-        continue
-
-    if val == '>':
-        in_garbage = False
-        continue
-
-    if val in ['{','}'] and not in_garbage:
-        clean_string.append(val)
-    
-    if in_garbage and val != '*':
-        non_junk += 1
-
-
 layer = 0
 count = 0
 
-for val in clean_string:
-    if val == "{":
+in_garbage = False
+
+for idx, val in enumerate(data):
+    if val == '!':
+        data[idx + 1] = '*'
+
+    elif val == '<' and not in_garbage:
+        in_garbage = True
+
+    elif val == '>':
+        in_garbage = False
+
+    elif val == '{' and not in_garbage:
         layer += 1
         count += layer
-    if val == "}":
+
+    elif val == '}' and not in_garbage:
         layer -= 1
 
-print(count)
-print(non_junk)
+    elif in_garbage and val != '*':
+        non_junk += 1
+
+print(f'Solution to Part 1: {count}')
+print(f'Solution to Part 2: {non_junk}')
